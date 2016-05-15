d3723c030d72b5d6dd8019e5b0a1d5175557a814
yang123456789 <1877084583@qq.com>
2016-05-15 15:49:10 +0800

    新建并完善了注册登录页面

---
+++ db/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Register',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('customer_username', models.CharField(max_length=255, db_index=True)),
+                ('customer_phone', models.CharField(max_length=255)),
+                ('customer_password', models.CharField(max_length=255)),
+                ('customer_again_password', models.CharField(max_length=255)),
+                ('identify_code', models.CharField(max_length=255)),
+            ],
+        ),
+    ]
--- db/models.py
+++ db/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Register(models.Model):
+    customer_username = models.CharField(max_length=255, null=False, db_index=True)
+    customer_phone = models.CharField(max_length=255, null=False)
+    customer_password = models.CharField(max_length=255, null=False)
+    customer_again_password = models.CharField(max_length=255, null=False)
+    identify_code = models.CharField(max_length=255, null=False)
+
+
 # Create your models here.
