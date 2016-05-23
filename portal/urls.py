97dd3da1cf6372b1f9887e6ffc0d75cbc7a9363d
yang123456789 <1877084583@qq.com>
2016-05-23 22:46:40 +0800

    增加了共同页面的css样式

--- portal/urls.py
+++ portal/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('portal.controllers',
     url(r'^/?$', 'resigter.index'),
     url(r'^register$', 'resigter.resigter'),
+    url(r'^common$', 'common.index'),
 )
