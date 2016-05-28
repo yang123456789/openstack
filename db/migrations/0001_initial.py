cd188fa8fd826fa7c9c14fb0e7e0d1f0f323eb32
yang123456789 <1877084583@qq.com>
2016-05-29 00:31:13 +0800

    增加了登录页面的jq验证并写了相应的后台程序

--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -14,6 +14,8 @@ class Migration(migrations.Migration):
             name='Register',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('auth_token', models.CharField(max_length=255)),
+                ('register_time', models.DateTimeField(auto_now_add=True)),
                 ('customer_username', models.CharField(max_length=255, db_index=True)),
                 ('customer_phone', models.CharField(max_length=255)),
                 ('customer_password', models.CharField(max_length=255)),
