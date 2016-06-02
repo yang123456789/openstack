6b2327a0e982073a9e7ef95d8f132544a8a04aee
yang123456789 <1877084583@qq.com>
2016-06-02 21:45:08 +0800

    添加了一些验证

--- portal/controllers/login.py
+++ portal/controllers/login.py
@@ -1,14 +1,19 @@
 from db.models import Register
 from db.views import *
+from portal import exception
 from portal import sshkey
 
 def validate(request):
     params = request.POST
     username = utf8(params['username'])
     vitify = Register.objects.filter(customer_username = username)
-    print vitify
-
-    if len(vitify) > 1:
-        raise
+    # print type(vitify)
+    # print vitify
+    if vitify:
+        if len(vitify) > 1:
+            raise exception.VitityLenException()
+        else:
+            for vf in vitify:
+                return vf.customer_username, vf.customer_password
     return render(request, 'homepage/login.html')
     # return redirect('')
