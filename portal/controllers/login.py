095a8b20e802a26a4246968b82e75739a150f4c1
yang123456789 <1877084583@qq.com>
2016-06-03 23:05:56 +0800

    增加了登录验证

--- portal/controllers/login.py
+++ portal/controllers/login.py
@@ -3,17 +3,22 @@ from db.views import *
 from portal import exception
 from portal import sshkey
 
+
 def validate(request):
     params = request.POST
     username = utf8(params['username'])
     vitify = Register.objects.filter(customer_username = username)
-    # print type(vitify)
-    # print vitify
     if vitify:
         if len(vitify) > 1:
             raise exception.VitityLenException()
         else:
             for vf in vitify:
-                return vf.customer_username, vf.customer_password
-    return render(request, 'homepage/login.html')
+                if vf.customer_username is not None and vf.customer_password is not None:
+                    ssh_public_key = sshkey.generation_two_keys(vf.customer_username, vf.customer_password)
+                    auth_token = sshkey.validation(ssh_public_key)
+                    if auth_token == vf.auth_token:
+                        return redirect('/common')
+                    else:
+                        raise exception.AuthTokenVitity()
+    # return render(request, 'homepage/login.html')
     # return redirect('')
