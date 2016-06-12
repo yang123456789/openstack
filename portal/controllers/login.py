5c2ec98f5b2971c4f9abc675fb039c4895de4614
yang123456789 <1877084583@qq.com>
2016-06-12 22:53:43 +0800

    修改了token验证

--- portal/controllers/login.py
+++ portal/controllers/login.py
@@ -2,19 +2,23 @@ from db.models import Register
 from db.views import *
 from portal import exception
 from portal import sshkey
+from django.http import HttpResponse
 
 
 def validate(request):
     params = request.POST
-    username = params.get('username')
-    password = params.get('password')
+    username = params['username']
+    password = params['password']
     vitify = Register.objects.filter(customer_username = username)
-    for vf in vitify:
-        customer_username = vf.customer_username
-        customer_password = vf.customer_password
-        if username == customer_username and password == customer_password:
-            print vf.auth_token
-        else:
-            raise exception.AuthTokenVitity()
-    return render(request, 'homepage/login.html')
+    if len(vitify) == 1:
+        for vf in vitify:
+            customer_username = vf.customer_username
+            customer_password = vf.customer_password
+            if username == customer_username and password == customer_password:
+                return redirect('/common', {'token': vf.auth_token})
+            else:
+                raise exception.AuthTokenVitity()
+    else:
+        raise exception.VitityLenException()
+    # return render(request, 'homepage/login.html')
     # return redirect('')
