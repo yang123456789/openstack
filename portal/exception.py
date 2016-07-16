a610e18461ba8152e029ee3dcf77caf4ac4db686
yang123456789 <1877084583@qq.com>
2016-07-16 23:00:35 +0800

    弯身了注册功能

--- portal/exception.py
+++ portal/exception.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext as _
 
 
 class OpenstackException(Exception):
-    msg_fmt = ('OpenstackException')
+    msg_fmt = _('OpenstackException')
 
     def __init__(self, message=None):
         if not message:
@@ -15,8 +15,8 @@ class OpenstackException(Exception):
 
 
 class VitityLenException(OpenstackException):
-    msg_fmt = ('Register list can not more than One')
+    msg_fmt = _('Register list can not more than One')
 
 
 class AuthTokenVitity(OpenstackException):
-    msg_fmt = ('AuthToken vititied invalid')
+    msg_fmt = _('AuthToken vititied invalid')
--- portal/urls.py
+++ portal/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('portal.controllers',
-    # url(r'^$', 'register.index'),
+    # url(r'^$', 'login.index'),
     # url(r'^login$', 'login.validate'),
     url(r'^register$', 'register.register'),
     url(r'^common$', 'common.index'),
