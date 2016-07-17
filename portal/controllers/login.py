f957eda15222e195a55e70e821255b381a286a5e
yang123456789 <1877084583@qq.com>
2016-07-17 21:07:02 +0800

    增加了验证码

---
+++ portal/controllers/login.py
@@ -0,0 +1,13 @@
+from db.forms import LoginForm
+from db.models import Register
+from db.views import *
+from django.utils.translation import ugettext as _
+
+
+def index(request):
+    return render(request, 'sysadmin/login.html')
+
+
+def login(request):
+    session = request.session
+    return
