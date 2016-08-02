e162023a18392d0c9f59e4eee1e3a267a5e6b5fb
yang123456789 <1877084583@qq.com>
2016-08-02 23:00:12 +0800

    修改了登录界面

--- static/js/login/login.js
+++ static/js/login/login.js
@@ -1,44 +1,25 @@
 (function($) {
     openstack_login = function() {
-        var username = $('.user').val();
-        var password = $('.password').val();
-        var error = ['用户名不能为空!', '密码不能为空!'];
-        var span_error = $('.error');
-        if (username == "") {
-            span_error[0].innerHTML = error[0];
-            return false;
-        }
-        if (password == "" && username != "") {
-            span_error[0].innerHTML = error[1];
-            return false;
-        }
-        return true;
-    };
-    transmission_data = function() {
-        //var username = $('.user').val();
-        //var password = $('.password').val();
-        //var params = {
-        //    'username': username,
-        //    'password': password
-        //};
-        $ajax({
-            type: "post",
-            url: "/login",
-            data: {'username': customer_username, 'password': customer_password},
-            success: function(msg) {
-                console.log('qwe')
-                $('form').form(msg)
-            },
-            error: function(data) {
-                console.log('qwe')
-                var span_error = $('.error');
-                span_error[0].innerHTML = '用户名或密码错误!';
-                return false;
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var code = $('input[name="code"]').val();
+        var params = {
+            'username': username,
+            'password': password,
+            'code': code
+        };
+        $.ajax({
+            type: "GET",
+            url: "login/validate",
+            data: params,
+            success: function(result) {
+                if(result['data'].indexOf(username) == -1) {
+
+                }
+                if(result['data'].indexOf(password) == -1) {
+
+                }
             }
         });
     };
-    disappear = function() {
-        var span_error = $('.error');
-        span_error[0].innerHTML = '';
-    }
 })(jQuery);
