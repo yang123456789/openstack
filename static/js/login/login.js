365f3a4063bd3f1022bb533a9bb0a2097a170502
yang123456789 <1877084583@qq.com>
2016-08-11 22:46:13 +0800

    修改了登录的js代码

--- static/js/login/login.js
+++ static/js/login/login.js
@@ -1,12 +1,41 @@
 (function($) {
-    openstack_login = function() {
+    login = function() {
         var username = $('input[name="username"]').val();
         var password = $('input[name="password"]').val();
-        var code = $('input[name="code"]').val();
+        var captcha = $('input[name="captcha"]').val();
+        if (validate_inputs(username, password, captcha)) {
+            do_login(username, password, captcha)
+        }
+    };
+
+    validate_inputs = function(username, password, captcha) {
+        if (username == '') {
+            render_error('用户名不能为空');
+            $('#username').focus();
+            return false;
+        }
+        if (password == '') {
+            render_error('密码不能为空');
+            $('#password').focus();
+            return false;
+        }
+        if (captcha == '') {
+            render_error('验证码不能为空');
+            $("#J_codetext").focus();
+            return false;
+        }
+        if (captcha.length < 4 || captcha.length > 4) {
+            render_error('验证码错误')
+            $("#J_codetext").focus();
+            return false;
+        }
+    };
+
+    do_login = function(username, password, captcha) {
         var params = {
             'username': username,
             'password': password,
-            'code': code
+            'captcha': captcha
         };
         $.ajax({
             type: "GET",
@@ -14,12 +43,18 @@
             data: params,
             success: function(result) {
                 if(result['data'].indexOf(username) == -1) {
-
+                    $('#info').html('用户名不正确')
                 }
-                if(result['data'].indexOf(password) == -1) {
-
+                else if(result['data'].indexOf(password) == -1) {
+                    $('#info').html('密码不正确')
                 }
             }
         });
-    };
+    }
+
+    render_error = function(error_message) {
+        var elem_error = $('#error_message');
+        elem_error.html(error_message);
+        elem_error.show();
+    }
 })(jQuery);
